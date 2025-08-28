e838eb62178c33bd90e8f57b56d928db7d54afb4
Divya-Tommkuntla <toomkuntladivyareddy@gmail.com>
2025-08-28 15:30:44 +0530

    Initial commit - DNA cryptography project

---
+++ DATABASE/dna_impl.sql
@@ -0,0 +1,101 @@
+/*
+SQLyog Enterprise - MySQL GUI v6.56
+MySQL - 5.0.67-community-nt : Database - dna_impl
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`dna_impl` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `dna_impl`;
+
+/*Table structure for table `admin` */
+
+DROP TABLE IF EXISTS `admin`;
+
+CREATE TABLE `admin` (
+  `username` varchar(100) default NULL,
+  `password` varchar(100) default NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `admin` */
+
+insert  into `admin`(`username`,`password`) values ('admin','admin');
+
+/*Table structure for table `cloud` */
+
+DROP TABLE IF EXISTS `cloud`;
+
+CREATE TABLE `cloud` (
+  `username` varchar(100) default NULL,
+  `password` varchar(100) default NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `cloud` */
+
+insert  into `cloud`(`username`,`password`) values ('Cloud','Cloud');
+
+/*Table structure for table `dna` */
+
+DROP TABLE IF EXISTS `dna`;
+
+CREATE TABLE `dna` (
+  `id` int(11) NOT NULL auto_increment,
+  `sender` varchar(1000) default NULL,
+  `receiver` varchar(1000) default NULL,
+  `message` varchar(1000) default NULL,
+  `ascii` varchar(100) default NULL,
+  `hexa` varchar(1000) default NULL,
+  `binary` longtext,
+  `dna` longtext,
+  `date` varchar(100) default NULL,
+  `decode` varchar(100) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*Data for the table `dna` */
+
+/*Table structure for table `receiver` */
+
+DROP TABLE IF EXISTS `receiver`;
+
+CREATE TABLE `receiver` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(100) default NULL,
+  `email` varchar(100) default NULL,
+  `mobile` varchar(100) default NULL,
+  `address` varchar(1000) default NULL,
+  `username` varchar(1000) default NULL,
+  `password` varchar(1000) default NULL,
+  `status` varchar(1000) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `receiver` */
+
+/*Table structure for table `sender` */
+
+DROP TABLE IF EXISTS `sender`;
+
+CREATE TABLE `sender` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(100) default NULL,
+  `email` varchar(100) default NULL,
+  `mobile` varchar(100) default NULL,
+  `address` varchar(1000) default NULL,
+  `username` varchar(1000) default NULL,
+  `password` varchar(1000) default NULL,
+  `status` varchar(1000) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*Data for the table `sender` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
